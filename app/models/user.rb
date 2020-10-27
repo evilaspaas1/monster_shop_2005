c1ed582518241f71123a28c2a1efe9d1c1cac4ea
James Belta <james.belta@gmail.com>
2020-10-27 17:50:59 -0600

    roles default, employee, admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  enum role: %w(default employee admin)
 
   has_secure_password
 end
