8c24ef2c2b29b3fdd7e3aa1973d6bcfa28fe46f0
Austin Aspaas <araspaas@live.com>
2020-10-31 17:57:27 -0600

    Add ship action to admin dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,12 @@ class Admin::DashboardController < Admin::BaseController
     @orders = Order.all
   end
 
+  def ship
+    order = Order.find(params[:order_id])
+    order.update!(status: "shipped")
+    redirect_to admin_path
+  end
+
   def users_index
 
   end
