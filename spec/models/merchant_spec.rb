769daf2119304e965393342b28fa79ca18cc62bf
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-02 20:10:03 -0700

    Add needed objects for model spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -67,6 +67,26 @@ describe Merchant, type: :model do
       expect(@meg.distinct_cities).to include("Denver")
       expect(@meg.distinct_cities).to include("Hershey")
     end
+    it 'pending orders' do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      pull_toy = brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      fred = User.create(name: "Fred Savage",
+                   address: "666 Devil Ave",
+                   city: "Mesatown",
+                   state: "AZ",
+                   zip: '80085',
+                   email: "randogmail.com",
+                   password: "test",
+                   role: 0)
+      order_1 = fred.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: "pending")
+      order_2 = fred.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: "cancelled")
+      order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 'pending')
+      order_1.item_orders.create!(item: pull_toy, price: pull_toy.price, quantity: 3, status: 'fulfilled')
+      order_2.item_orders.create!(item: pull_toy, price: pull_toy.price, quantity: 5, status: 'fulfilled')
 
+      expect(meg.pending_orders).to eq([order_1])
+    end 
   end
 end
